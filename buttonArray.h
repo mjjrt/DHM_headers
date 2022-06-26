0061264854e58bcd84536a4bcdc9f464d8ff8ac0
mjjrt <64268845+mjjrt@users.noreply.github.com>
2022-06-26 17:49:32 +0200

    Initial Commit

---
+++ buttonArray.h
@@ -0,0 +1,67 @@
+#include <Button.h>
+
+#define NUM_BUTTONS 12
+const int buttonPins[] = {34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 52, 53}; // Button Pins 
+
+const int ledPin1 = 44;      // the number of the LED pin
+const int ledPin2 = 45;
+
+
+Button button1( buttonPins[0] );
+Button button2( buttonPins[1] );
+Button button3( buttonPins[2] );
+Button button4( buttonPins[3] );
+Button button5( buttonPins[4] );
+Button button6( buttonPins[5] );
+Button button7( buttonPins[6] );
+Button button8( buttonPins[7] );
+Button button9( buttonPins[8] );
+Button button0( buttonPins[9] );
+Button buttonstar( buttonPins[10] );
+Button buttonhash( buttonPins[11] );
+
+
+void ButtonSetup()
+{
+  button1.begin();
+  button2.begin();
+  button3.begin();
+  button4.begin();
+  button5.begin();
+  button6.begin();
+  button7.begin();
+  button8.begin();
+  button9.begin();
+  button0.begin();
+  buttonstar.begin();
+  buttonhash.begin();
+}
+
+int CheckButtonStates()
+{
+  if (button1.pressed())
+    return 0;
+  else if (button2.pressed())
+    return 1;
+  else if (button3.pressed())
+    return 2; 
+  else if (button4.pressed())
+    return 3;
+  else if (button5.pressed())
+    return 4;
+  else if (button6.pressed())
+    return 5;
+  else if (button7.pressed())
+    return 6;
+  else if (button8.pressed())
+    return 7;
+  else if (button9.pressed())
+    return 8;
+  else if (button0.pressed())
+    return 9;
+  else if (buttonstar.pressed())
+    return 10;
+  else if (buttonhash.pressed())
+    return 11;
+  else return -1;
+}
